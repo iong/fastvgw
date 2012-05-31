461838755a3d0b6b5e69e86afb187f3b72b6f117
Ionut Georgescu <ionut.georgescu@uci.edu>
2012-05-31 16:01:37 -0700

    New run script

--- run.sh
+++ run.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-print_cfg () {
-cat << EOF
-250 250 0.35 0.35 1.3 20
-250 250 0.3 0.3 1.25 10
-250 250 0.2 0.2 1.15 50
-150 350 0.06 0.60 1.00 150
-150 350 0.06 0.60 0.9 400
-50 450 0.001 0.6 0.75 2000
-EOF
-}
-
-save_data() {
-	d=$1
-	rm -rf $d
-	mkdir $d
-	cat Um_*.dat > $d/Um.dat
-	mv Um_*.dat *.log $d
-}
-
-echo "#!/bin/bash" > tmp_script
-print_cfg | sed -e 's/^/.\/gibbs3 /' -e 's/$/ \&/' >> tmp_script
-echo "wait" >> tmp_script
-chmod 755 tmp_script
-exit
-./tmp_script
-save_data disp+vol+swap@dest
+export OMP_NUM_THREADS=8
+for LAMBDA in 0.09; do
+#for LAMBDA in 0.03 0.06 0.09 0.12 0.15 0.19 0.21; do
+	rm mdp_deBoer=${LAMBDA}.dat
+	for f in `ls *.mdp?.xyz | sort -n`; do
+		echo "deBoer = $LAMBDA $f" 1>&2
+		./clustergs $f $LAMBDA >> mdp_deBoer=${LAMBDA}.dat
+	done
+	rm ticos_deBoer=${LAMBDA}.dat
+	for f in `ls *.ticos.xyz| sort -n`; do
+		echo "deBoer = $LAMBDA $f" 1>&2
+		./clustergs $f $LAMBDA >> ticos_deBoer=${LAMBDA}.dat
+	done
+done
